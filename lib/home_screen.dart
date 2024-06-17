70ba1c95ab379ab234829018c3ff5895af4cfba6
Md Apon Ahmed <mdaponahmed412@gmail.com>
2024-06-17 23:41:58 +0600

    simple text show

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_state_management/main.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends ConsumerWidget { // How Read the value. first Convert ConsumerWidget
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
+  Widget build(BuildContext context, WidgetRef ref) { // Then requred WidgetRef Set 
+  final name =  ref.watch(nameProvider); //! this watch is read variable and this time nameProvider and name is variable 
+    return  Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text(name), // and show this name
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_state_management/home_screen.dart';
 
+final nameProvider = Provider<String>/*Data Type Good Practices*/((ref) {
+  //!  provider requred a function this function own a value and return this null value ....
+  return 'Apon';
+});
 void main() {
   runApp(
-    const ProviderScope( // ProviderScope is very important other wise error show !!
+    const ProviderScope(
+      // ProviderScope is very important other wise error show !!
       child: MyApp(),
     ),
   );
